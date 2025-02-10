ff08586053ef51d4df5c42bfb8e5d925df6f30f5
Ramil <zarram89@gmail.com>
2025-02-10 14:30:31 +0500

    15-16 Form and validation

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -9,7 +9,8 @@ class Job extends Model {
   use HasFactory;
   protected $table = 'job_listings';
 
-  protected $fillable = ['title', 'salary'];
+  // protected $fillable = ['title', 'salary', 'employer_id'];
+  protected $guarded = [];
 
   public function employer() {
     return $this->belongsTo(Employer::class);
--- routes/web.php
+++ routes/web.php
@@ -11,22 +11,42 @@ Route::get('/', function () {
 Route::get('/jobs', function () {
   // $jobs = Job::all();
   // Так можно переходить на конкретную страницу
-  $jobs = Job::with('employer')->simplePaginate(10);
+  $jobs = Job::with('employer')->latest()->simplePaginate(20);
 
   //А так показывается набор, по которому не найти конкретную страницу
   // $jobs = Job::with('employer')->cursorPaginate(5);
   // $jobs = Job::all();
 
-  return view('jobs', [
+  return view('jobs.index', [
     'jobs' => $jobs
   ]);
 });
 
+Route::get("/jobs/create", function () {
+  return view('jobs.create');
+});
 
 Route::get('/jobs/{id}', function ($id) {
   $job = Job::find($id);
 
-  return view('job', ['job' => $job]);
+  return view('jobs.show', ['job' => $job]);
+});
+
+Route::post('/jobs', function () {
+  // dd(request()->all()); - вывести все данные из запроса
+  // dd(request('title')); - вывести заголовок из запроса
+  request()->validate(  [
+    'title'=> ['required', 'min:3'],
+    'salary'=> ['required'],
+  ]);
+
+  Job::create([
+    'title'=> request('title'),
+    'salary' => request('salary'),
+    'employer_id'=> 1,
+  ]);
+
+  return redirect('/jobs');
 });
 
 Route::get('/about', function () {
